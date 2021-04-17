80edc36f0c2c7126860977652f5a34d52fa36c44
Deadshadow432 <randoni743@gmail.com>
2021-04-17 20:41:19 +0300

    Added navigation buttons

--- app/src/main/java/com/example/vegainz/HomeFragment.java
+++ app/src/main/java/com/example/vegainz/HomeFragment.java
@@ -1,10 +1,19 @@
 package com.example.vegainz;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -12,6 +21,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -47,16 +57,66 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = Navigation.findNavController(view);
+
+        Button DI = view.findViewById(R.id.buttonDietInput);
+        DI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_homeFragment_to_dietInputFragment);
+            }
+        });
+
+        Button DC = view.findViewById(R.id.buttonDietChange);
+        DC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_homeFragment_to_dietChangeFragment);
+            }
+        });
+
+        Button MI = view.findViewById(R.id.buttonMassInput);
+        MI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_homeFragment_to_massInputFragment);
+            }
+        });
+
+        Button MC = view.findViewById(R.id.buttonMassChange);
+        MC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_homeFragment_to_massChangeFragment);
+            }
+        });
+
+        Button LO = view.findViewById(R.id.buttonLogout);
+        LO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_homeFragment_to_loginFragment);
+            }
+        });
+    }
+    
 }
