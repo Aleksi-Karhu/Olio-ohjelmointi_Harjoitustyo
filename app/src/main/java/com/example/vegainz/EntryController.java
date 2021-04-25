3caa23b3382d27d8de4e8c6d674421e0fde726fb
Deadshadow432 <randoni743@gmail.com>
2021-04-25 21:01:56 +0300

    Mass input submit v.000121

--- app/src/main/java/com/example/vegainz/EntryController.java
+++ app/src/main/java/com/example/vegainz/EntryController.java
@@ -1,11 +1,24 @@
 package com.example.vegainz;
 
+import java.util.ArrayList;
+
 public class EntryController {
+    private final ArrayList<Entry> entries = new ArrayList();
 
+    public void createMassEntry(String date, float mass){
+        entries.add(new MassEntry(date,mass));
+    }
 
-
-
-
+    public void printEntries(){
+        System.out.println("All accounts:");
+        Entry temp;
+        if(entries != null){
+            for(int i = 0;i<entries.size();i++) {
+                temp = entries.get(i);
+                System.out.println("Date: "+temp.date+" Mass: "+ ((MassEntry) temp).mass);
+                }
+            }
+    }
 
 
 }
