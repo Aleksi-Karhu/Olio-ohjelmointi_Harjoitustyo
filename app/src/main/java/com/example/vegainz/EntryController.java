3d79b7e0643467b09263c082bcf5071a5ec13cc1
Aleksi-Karhu <aleksi.karhu.mail@gmail.com>
2021-04-27 22:44:32 +0300

    Added database

--- app/src/main/java/com/example/vegainz/EntryController.java
+++ app/src/main/java/com/example/vegainz/EntryController.java
@@ -1,16 +1,79 @@
 package com.example.vegainz;
 
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
 
 public class EntryController {
     private final static ArrayList<Entry> entries = new ArrayList();
 
+    private static final String DATE_KEY = "date";
+    private static final String MASS_KEY = "mass";
+    private static final String TAG = "mass";
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("massEntries/userMass");
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference massRef = db.collection("massEntries");
+
+
+
+    public void makeMassEntry(String date, float mass) throws ParseException {
+        MassEntry massentry = new MassEntry(date, mass);
+
+        massRef.add(massentry).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+
+            }
+        });
+
+    }
+
+
+
     public void createMassEntry(String date, float mass) throws ParseException {
+        Map<String, Object> saveMass = new HashMap<String, Object>();
+        saveMass.put(DATE_KEY, date);
+        saveMass.put(MASS_KEY, mass);
+        mDocRef.set(saveMass).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "Saving entry successful ");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Saving entry failed");
+            }
+        });
 
         entries.add(new MassEntry(date,mass));
     }
+
+    public void getMassEntry() {
+        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+            }
+        });
+
+    }
+
+
 
     public void createFoodCalculationEntry(String diet, boolean lowCarbonPreference,
                                            float beef, float fish, float pork, float dairy,
